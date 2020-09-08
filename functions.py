d2ff24b883e41c77405e301eaac84c7abf00b42e
H-Jan <69823896+H-Jan@users.noreply.github.com>
2020-09-08 13:48:53 -0700

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,51 @@
+#Question 1
+def area(width, height):
+    result = width * height
+    return result
+
+#Question 2
+result = area(5, 6)
+print(result)
+
+#Question 3
+result = area(7, 3)
+print(result)
+
+result = area(19, 29)
+print(result)
+
+#Question 4
+def subtract(number1, number2):
+    solution = number1 - number2
+    return solution
+
+#Question 5
+MathProblem = subtract(31, 42)
+print(MathProblem)
+
+AnotherProblem = subtract(442, 44)
+print(AnotherProblem)
+#In these two examples, I simply wanted to call a differently defined variable
+
+#Question 6
+#Answered in Comments
+
+#Question 7
+def addition(num1, num2): 
+    answer = num1 + num2
+    return answer
+
+print (addition(1, 2))
+#I wanted to try one line of code to yield the result
+
+answer = addition(4, 9)
+print(answer)
+
+
+
+
+
+
+
+
+
